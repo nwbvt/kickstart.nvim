e048702d572c1970a5002e596e67b4534c23237e
Nicholas Brown <nwbrown@gmail.com>
2025-02-13 16:12:37 -0500

    set tab width to 2

--- init.lua
+++ init.lua
@@ -157,6 +157,10 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
